582f37fe6b171e53115ee985c9aca721da41eb61
Rafael Pallisser de Oliveira <rafaelpallisser@gmail.com>
2025-02-03 00:32:32 -0300

    adicionado exercicio 1

--- exercicios.py
+++ exercicios.py
@@ -1,6 +1,11 @@
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+soma = n1 + n2
+print(soma)
+
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
