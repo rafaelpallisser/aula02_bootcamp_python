edb7d524044371da1827a4e743647f444825f8cf
Rafael Pallisser de Oliveira <rafaelpallisser@gmail.com>
2025-02-03 00:51:52 -0300

    adicionado exercicio 4

--- exercicios.py
+++ exercicios.py
@@ -11,12 +11,16 @@
 # print(numero % 5)
 
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
-n1 = float(input("Digite o primeiro número: "))
-n2 = float(input("DIgite o segundo número: "))
-multiplicacao = n1 * n2
-print(multiplicacao)
+# n1 = float(input("Digite o primeiro número: "))
+# n2 = float(input("DIgite o segundo número: "))
+# multiplicacao = n1 * n2
+# print(multiplicacao)
 
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("DIgite o segundo número: "))
+print(n1 // n2)
+
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
 # #### Números de Ponto Flutuante (`float`)
